48d5c320d22d370280d63fb948a4a6205ade7dc0
wiktorszczepanik <wiktorszczepanik036@gmail.com>
2025-06-12 00:16:11 +0200

    Update MS Drop

--- MSSQL/MS-DROP.sql
+++ MSSQL/MS-DROP.sql
@@ -1,74 +1,79 @@
--- DROP FOREIGN KEYS
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2025-06-11 22:05:32.922
 
-IF OBJECT_ID('dbo.account_consents', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.account_consents DROP CONSTRAINT FK_account_consents_account;
-    ALTER TABLE dbo.account_consents DROP CONSTRAINT FK_account_consents_consents;
-END;
+-- foreign keys
+ALTER TABLE account DROP CONSTRAINT account_account_number_format;
 
-IF OBJECT_ID('dbo.account', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.account DROP CONSTRAINT FK_account_account_number_format;
-    ALTER TABLE dbo.account DROP CONSTRAINT FK_account_client_data;
-    ALTER TABLE dbo.account DROP CONSTRAINT FK_account_currency;
-    ALTER TABLE dbo.account DROP CONSTRAINT FK_account_status;
-END;
+ALTER TABLE account DROP CONSTRAINT account_client_data;
 
-IF OBJECT_ID('dbo.activity_log', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.activity_log DROP CONSTRAINT FK_activity_action_type;
-    ALTER TABLE dbo.activity_log DROP CONSTRAINT FK_activity_session_history;
-END;
+ALTER TABLE account DROP CONSTRAINT account_currency;
 
-IF OBJECT_ID('dbo.address', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.address DROP CONSTRAINT FK_address_address_type;
-    ALTER TABLE dbo.address DROP CONSTRAINT FK_address_city;
-    ALTER TABLE dbo.address DROP CONSTRAINT FK_address_client_data;
-END;
+ALTER TABLE client_data DROP CONSTRAINT account_info_prefered_contact;
 
-IF OBJECT_ID('dbo.city', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.city DROP CONSTRAINT FK_city_country;
-END;
+ALTER TABLE account DROP CONSTRAINT account_status;
 
-IF OBJECT_ID('dbo.client_data', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.client_data DROP CONSTRAINT FK_client_data_preferred_contact;
-    ALTER TABLE dbo.client_data DROP CONSTRAINT FK_client_data_status;
-END;
+ALTER TABLE activity_log DROP CONSTRAINT activity_action_type;
 
-IF OBJECT_ID('dbo.financial_log', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.financial_log DROP CONSTRAINT FK_financial_log_account;
-    ALTER TABLE dbo.financial_log DROP CONSTRAINT FK_financial_log_account_number_format;
-    ALTER TABLE dbo.financial_log DROP CONSTRAINT FK_financial_log_currency;
-    ALTER TABLE dbo.financial_log DROP CONSTRAINT FK_financial_log_transaction_type;
-END;
+ALTER TABLE activity_log DROP CONSTRAINT activity_session_history;
 
-IF OBJECT_ID('dbo.session_history', 'U') IS NOT NULL
-BEGIN
-    ALTER TABLE dbo.session_history DROP CONSTRAINT FK_session_history_account;
-END;
+ALTER TABLE address DROP CONSTRAINT address_address_type;
 
+ALTER TABLE address DROP CONSTRAINT address_city;
 
--- DROP TABLES
+ALTER TABLE address DROP CONSTRAINT address_client_data;
 
-IF OBJECT_ID('dbo.account_consents', 'U') IS NOT NULL DROP TABLE dbo.account_consents;
-IF OBJECT_ID('dbo.activity_log', 'U') IS NOT NULL DROP TABLE dbo.activity_log;
-IF OBJECT_ID('dbo.address', 'U') IS NOT NULL DROP TABLE dbo.address;
-IF OBJECT_ID('dbo.city', 'U') IS NOT NULL DROP TABLE dbo.city;
-IF OBJECT_ID('dbo.client_data', 'U') IS NOT NULL DROP TABLE dbo.client_data;
-IF OBJECT_ID('dbo.financial_log', 'U') IS NOT NULL DROP TABLE dbo.financial_log;
-IF OBJECT_ID('dbo.session_history', 'U') IS NOT NULL DROP TABLE dbo.session_history;
-IF OBJECT_ID('dbo.account', 'U') IS NOT NULL DROP TABLE dbo.account;
-IF OBJECT_ID('dbo.account_number_format', 'U') IS NOT NULL DROP TABLE dbo.account_number_format;
-IF OBJECT_ID('dbo.action_type', 'U') IS NOT NULL DROP TABLE dbo.action_type;
-IF OBJECT_ID('dbo.address_type', 'U') IS NOT NULL DROP TABLE dbo.address_type;
-IF OBJECT_ID('dbo.consents', 'U') IS NOT NULL DROP TABLE dbo.consents;
-IF OBJECT_ID('dbo.country', 'U') IS NOT NULL DROP TABLE dbo.country;
-IF OBJECT_ID('dbo.currency', 'U') IS NOT NULL DROP TABLE dbo.currency;
-IF OBJECT_ID('dbo.preferred_contact', 'U') IS NOT NULL DROP TABLE dbo.preferred_contact;
-IF OBJECT_ID('dbo.status', 'U') IS NOT NULL DROP TABLE dbo.status;
-IF OBJECT_ID('dbo.transaction_type', 'U') IS NOT NULL DROP TABLE dbo.transaction_type;
+ALTER TABLE city DROP CONSTRAINT city_country;
+
+ALTER TABLE account_consents DROP CONSTRAINT client_consents_client_account;
+
+ALTER TABLE account_consents DROP CONSTRAINT client_consents_consents;
+
+ALTER TABLE client_data DROP CONSTRAINT client_data_status;
+
+ALTER TABLE financial_log DROP CONSTRAINT financial_log_account;
+
+ALTER TABLE financial_log DROP CONSTRAINT financial_log_an_format;
+
+ALTER TABLE financial_log DROP CONSTRAINT financial_log_currency;
+
+ALTER TABLE financial_log DROP CONSTRAINT financial_log_transaction_type;
+
+ALTER TABLE session_history DROP CONSTRAINT session_history_client_account;
+
+-- tables
+DROP TABLE account;
+
+DROP TABLE account_consents;
+
+DROP TABLE account_number_format;
+
+DROP TABLE action_type;
+
+DROP TABLE activity_log;
+
+DROP TABLE address;
+
+DROP TABLE address_type;
+
+DROP TABLE city;
+
+DROP TABLE client_data;
+
+DROP TABLE consents;
+
+DROP TABLE country;
+
+DROP TABLE currency;
+
+DROP TABLE financial_log;
+
+DROP TABLE preferred_contact;
+
+DROP TABLE session_history;
+
+DROP TABLE status;
+
+DROP TABLE "transaction";
+
+-- End of file.
 
