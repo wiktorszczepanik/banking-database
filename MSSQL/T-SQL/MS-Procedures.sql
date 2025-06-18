ecbdd9d26037455777429a051d1dc80e18407065
wiktorszczepanik <wiktorszczepanik036@gmail.com>
2025-06-18 18:48:40 +0200

    Small update

--- MSSQL/T-SQL/MS-Procedures.sql
+++ MSSQL/T-SQL/MS-Procedures.sql
@@ -3,18 +3,19 @@
 CREATE PROCEDURE updateLoyaltyRating
 AS BEGIN
     -- Initial loyalty rating for account
+    DECLARE @previousLoyaltyRating DECIMAL(5, 2) = 0.00;
     DECLARE @newLoyaltyRating DECIMAL(5, 2) = 0.00;
 
     -- Values for setting new loyalty rating
     DECLARE @currentAvailableFunds NUMERIC(19, 2);
-    DECLARE @currentYearSeniority DATE;
+    DECLARE @currentYearSeniority INT;
 
     DECLARE @currentBalanceStat SMALLINT;
     DECLARE @currentFinancialStat DECIMAL(5, 2);
     DECLARE @currentRegistrationDateStat DECIMAL(5, 2);
     DECLARE @currentAdditionalConsentsStat SMALLINT;
 
-    DECLARE curs1 CURSOR FOR SELECT id FROM account;
+    DECLARE curs1 CURSOR FOR SELECT id, status_id FROM account;
     DECLARE @currentAccountId BIGINT, @currentAccountStatus INT;
 
     OPEN curs1;
@@ -23,11 +24,18 @@ AS BEGIN
         -- Account status need to be "ACTIVE"
         IF @currentAccountStatus = 1 BEGIN
 
-            -- Available funds on account (0.00-30.00)
-            SET @currentAvailableFunds = (
-                SELECT available FROM account
+            -- Get loyalty rating before update
+            SET @previousLoyaltyRating = (
+                SELECT loyalty_rating FROM account
                 WHERE id = @currentAccountId
             );
+
+            -- Available funds on account (0.00-30.00)
+            SET @currentAvailableFunds = (
+                SELECT SUM(amount) FROM financial_log
+                WHERE account_id = @currentAccountId
+            );
+
             SET @currentBalanceStat = CASE -- category points
                 WHEN @currentAvailableFunds >= 0 AND @currentAvailableFunds < 10000 THEN 5
                 WHEN @currentAvailableFunds >= 10000 AND @currentAvailableFunds < 100000 THEN 10
@@ -54,8 +62,9 @@ AS BEGIN
                 FROM account
                 WHERE id = @currentAccountId
             );
+
             SET @currentRegistrationDateStat = CASE
-                WHEN @currentYearSeniority < 15 THEN (@currentYearSeniority / 15) * 30
+                WHEN @currentYearSeniority < 15 THEN (@currentYearSeniority / 15.00) * 30
                 ELSE 30
             END;
             SET @newLoyaltyRating = @newLoyaltyRating + @currentRegistrationDateStat;
@@ -71,16 +80,116 @@ AS BEGIN
                 AND c.mandatory = 0
             );
             SET @newLoyaltyRating = @newLoyaltyRating + @currentAdditionalConsentsStat;
-            
+
             -- UPDATE loyalty rating
             UPDATE account SET loyalty_rating = @newLoyaltyRating
             WHERE id = @currentAccountId;
+
+            -- Change to DBMS
+            PRINT 'Account ID: ' + CONVERT(VARCHAR, @currentAccountId)
+                + ' | (' + CONVERT(VARCHAR, @previousLoyaltyRating)
+                + ' -> ' + CONVERT(VARCHAR, @newLoyaltyRating) + ')'
         END;
+        SET @previousLoyaltyRating = 0.00;
         SET @newLoyaltyRating = 0.00;
         FETCH NEXT FROM curs1 INTO @currentAccountId, @currentAccountStatus;
     END;
+    CLOSE curs1;
+    DEALLOCATE curs1;
 END;
 
-/* Payment incident detector */
+EXEC updateLoyaltyRating;
 
+
+/* Financial statistics for account */
+
+CREATE PROCEDURE getFinancialStats
+    @inputAccountId BIGINT,
+    @startDate DATE,
+    @endDate DATE
+AS BEGIN
+
+    DECLARE @transactionBalance NUMERIC(19, 2);
+    DECLARE @accountCurrency CHAR(3);
+    DECLARE @transactionCount INT;
+    DECLARE @profits NUMERIC(19, 2);
+    DECLARE @expenses NUMERIC(19, 2);
+
+    -- Init account stats
+    PRINT '--- Financial Statistics --- ';
+    PRINT '<' + CONVERT(VARCHAR, @startDate) + ' ; ' + CONVERT(VARCHAR, @endDate) + '>'
+    PRINT 'Account ID: ' + CONVERT(VARCHAR, @inputAccountId);
+
+    SET @accountCurrency = (
+        SELECT c.currency_3
+        FROM account a
+        INNER JOIN currency c ON a.currency_id = c.id
+        WHERE a.id = @inputAccountId
+    );
+    SET @transactionBalance = (
+        SELECT SUM(amount) FROM financial_log
+        WHERE account_id = @inputAccountId
+        AND timestamp >= @startDate AND timestamp <= @endDate
+    );
+    PRINT 'Current balance: ' + CONVERT(VARCHAR, @transactionBalance) + ' ' + @accountCurrency;
+
+    SET @transactionCount = (
+        SELECT COUNT(amount) FROM financial_log
+        WHERE account_id = @inputAccountId
+        AND timestamp >= @startDate AND timestamp <= @endDate
+    );
+    PRINT 'Number of transactions: ' + CONVERT(VARCHAR, @transactionCount);
+
+    -- Expenses section
+    SET @expenses = (
+        SELECT SUM(amount) FROM financial_log
+        WHERE account_id = @inputAccountId AND amount < 0
+        AND timestamp >= @startDate AND timestamp <= @endDate
+    );
+    PRINT 'Expenses: ' + CONVERT(VARCHAR, ABS(@expenses)) + ' ' + @accountCurrency;
+    DECLARE curs1 CURSOR FOR SELECT t.type, SUM(amount)
+        FROM financial_log fl
+        INNER JOIN [transaction] t ON fl.transaction_type_id = t.id
+        WHERE fl.account_id = @inputAccountId AND amount < 0
+        AND timestamp >= @startDate AND timestamp <= @endDate
+        GROUP BY t.type
+        ORDER BY 2 DESC;
+    DECLARE @curs1Type VARCHAR(15), @curs1Sum NUMERIC(19, 2);
+    OPEN curs1;
+    FETCH NEXT FROM curs1 INTO @curs1Type, @curs1Sum;
+    WHILE @@FETCH_STATUS = 0 BEGIN
+        PRINT '----> ' + @curs1Type + ': ' + CONVERT(VARCHAR, ABS(@curs1Sum)) + ' ' + CONVERT(VARCHAR, @accountCurrency);
+        FETCH NEXT FROM curs1 INTO @curs1Type, @curs1Sum;
+    END;
+    CLOSE curs1;
+    DEALLOCATE curs1;
+
+    -- Profits section
+    SET @profits = (
+        SELECT SUM(amount) FROM financial_log
+        WHERE account_id = @inputAccountId AND amount > 0
+        AND timestamp >= @startDate AND timestamp <= @endDate
+    );
+    PRINT 'Profits: ' + CONVERT(VARCHAR, @profits) + ' ' + @accountCurrency;
+    DECLARE curs2 CURSOR FOR SELECT t.type, SUM(amount)
+        FROM financial_log fl
+        INNER JOIN [transaction] t ON fl.transaction_type_id = t.id
+        WHERE fl.account_id = @inputAccountId AND amount > 0
+        AND timestamp >= @startDate AND timestamp <= @endDate
+        GROUP BY t.type
+        ORDER BY 2 DESC;
+    DECLARE @curs2Type VARCHAR(15), @curs2Sum NUMERIC(19, 2);
+    OPEN curs2;
+    FETCH NEXT FROM curs2 INTO @curs2Type, @curs2Sum;
+    WHILE @@FETCH_STATUS = 0 BEGIN
+        PRINT '----> ' + @curs2Type + ': ' + CONVERT(VARCHAR, @curs2Sum) + ' ' + CONVERT(VARCHAR, @accountCurrency);
+        FETCH NEXT FROM curs2 INTO @curs2Type, @curs2Sum;
+    END;
+    CLOSE curs2;
+    DEALLOCATE curs2;
+
+END;
+
+
+EXEC getFinancialStats @inputAccountId = 4, @startDate = '2021-01-02 14:45:00.000', @endDate = '2025-05-10 15:30:00.000';
 
---
+++ MSSQL/T-SQL/MS-Triggers.sql
@@ -0,0 +1,197 @@
+/* Financial log trigger (append rush fee and validate session when inserting) */
+
+CREATE TRIGGER rushAndSession ON financial_log
+INSTEAD OF INSERT, UPDATE, DELETE
+AS BEGIN
+	-- Insert
+	IF EXISTS(SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted) BEGIN
+
+		-- Max id in financial log
+		DECLARE @maxFinancialLogId BIGINT;
+		SELECT @maxFinancialLogId = MAX(id) FROM financial_log;
+
+		-- Cursor setup
+		DECLARE curs1 CURSOR FOR SELECT account_id, amount, rush FROM inserted;
+		DECLARE @currentAccountId BIGINT, 
+			@currentAmount NUMERIC(19, 2), 
+			@currentRushStatus SMALLINT, 
+			@currentTimestamp DATETIME,
+			@currentTransactionType INT,
+			@currentDescription VARCHAR(1000),
+			@currentCurrencyId INT,
+			@currentCurrencyDate DATETIME,
+			@currentOtherAccountNumber VARCHAR(30),
+			@currentAccountNumberFormatId;
+
+		-- Help vars
+		DECLARE @rushFeeAmount NUMERIC(19, 2);
+		DECLARE @currentBalance NUMERIC(19, 2);
+		DECLARE @newRushState INT; -- 0 -> (not rush), 1 -> (rush request), 2+ -> (already validated rush) 
+		SET @newRushState = 0;
+
+		OPEN @curs1;
+		FETCH NEXT FROM curs1 INTO @currentAccountId, @currentAmount, @currentRushStatus, @currentTransactionType;   
+		WHILE @@Fetch_status = 0 BEGIN
+
+			-- Insert rush fee if new log has "rush" status (outcoming)
+			IF @currentRushStatus > 0 AND @currentAmount < 0 BEGIN
+
+				-- Calculate fee amount
+				SET @rushFeeAmount = 0.01 * @currentAmount;
+				IF @rushFeeAmount < 1.00 BEGIN
+					@rushFeeAmount = 1.00;
+				END;
+				ELSE IF @ rushFeeAmount > 10000.00 BEGIN
+					@rushFeeAmount = 10000.00;
+				END;
+
+				-- Insert fee amount
+				INSERT INTO financial_log (id, account_id, amount, rush, operation_date, [timestamp], transaction_type_id,
+					description, currency_id, currency_date, other_account_number, account_number_format_id)
+				VALUES (@maxFinancialLogId, @currentAccountId, -ABS(@rushFeeAmount), 0, GETDATE(), SYSDATETIME(), 
+					3, 'Rush fee', 1, SYSDATETIME(), 10000000000000000000000001, 1);
+				SET @maxFinancialLogId = @maxFinancialLogId + 1;
+				SET @newRushState = 2;
+			END;
+
+			-- Insert financial info
+			INSERT INTO financial_log (id, account_id, amount, rush, operation_date, [timestamp], transaction_type_id,
+					description, currency_id, currency_date, other_account_number, account_number_format_id)
+			VALUES (@maxFinancialLogId, @currentAccountId, @currentAmount, @newRushState, GETDATE(), @currentTimestamp, @currentTransactionType,
+					@currentDescription, @currentCurrency, @currentCurrencyDate, @currentOtherAccountNumber, @currentAccountNumberFormatId);
+			SET @maxFinancialLogId = @maxFinancialLogId + 1;
+			SET @newRushState = 0;
+
+			-- Update account balance for incoming financial info
+			IF @currentRushStatus > 0 AND @currentAmount > 0 BEGIN
+				SELECT @currentBalance = SUM(amount) 
+					FROM financial_log
+					WHERE account_id = @currentAccountId;
+				UPDATE account SET available = @currentBalance
+				WHERE id = @currentAccountId;
+			END;
+			FETCH NEXT FROM curs1 INTO @currentAccountId, @currentRushStatus;   
+		END;
+		CLOSE curs1;
+		DEALLOCATE curs1;
+
+	END;
+	-- Update
+	ELSE IF EXISTS(SELECT * FROM inserted) AND EXISTS(SELECT * FROM deleted) BEGIN
+		RAISERROR ('Cannot update financial logs.', 20, 1);
+	END;
+	-- Delete
+	ELSE IF EXISTS(SELECT * FROM deleted) AND NOT EXISTS(SELECT * FROM inserted) BEGIN
+		RAISERROR ('Cannot delete financial logs.', 21, 1);
+	END;
+END;
+
+
+
+/* Client data trigger (validate session when updating) */
+
+CREATE TRIGGER validateSessionForClientData ON client_data
+AFTER UPDATE, DELETE
+AS BEGIN
+	-- Update
+	IF EXISTS(SELECT * FROM inserted) AND EXISTS(SELECT * FROM deleted) BEGIN
+		-- Get current time
+		DECLARE @currentTime = SYSDATETIME();
+
+		-- Cursor setup
+		DECLARE curs1 CURSOR FOR SELECT id 
+			FROM deleted d
+			LEFT JOIN account a ON d.id = a.client_data_id
+			LEFT JOIN session_history sh ON a.id = sh.client_account_id
+			LEFT JOIN activity_log ac ON sh.id = ac.session_history_id;
+		DECLARE @currentId INT;
+		OPEN @curs1;
+	END;
+	-- Delete
+	ELSE IF EXISTS(SELECT * FROM deleted) AND NOT EXISTS(SELECT * FROM inserted) BEGIN
+
+		-- Reverse deletion
+		INSERT INTO client_data (id, first_name, middle_name, last_name, pesel, phone_code, phone_number, notification_phone_code, 
+			notification_phone_number, email, preferred_contact_id, status_id)
+		SELECT id, first_name, middle_name, last_name, pesel, phone_code, phone_number, notification_phone_code, 
+			notification_phone_number, email, preferred_contact_id, 2
+		FROM deleted;
+
+	END;
+END;
+
+
+
+/* Maintain primary address */
+
+CREATE TRIGGER maintainPrimaryAddress ON address
+AFTER INSERT, UPDATE, DELETE
+AS BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @id INT, @client_data_id INT;
+
+	-- Insert 
+	IF EXISTS(SELECT * FROM inserted) AND NOT EXISTS(SELECT * FROM deleted) BEGIN
+
+		-- Reset (1 -> 0) others if inserted is primary (1)
+		-- Only one primary per client is allowed
+		UPDATE address SET [primary] = 0
+        FROM address a
+        JOIN inserted i ON a.client_data_id = i.client_data_id
+        WHERE i.[primary] = 1 
+		AND a.id <> i.id 
+		AND a.[primary] = 1;
+    END;
+
+    --------------------------------------------------------------------------------
+    -- UPDATE: jeśli zmieniono primary → zresetuj inne
+    --------------------------------------------------------------------------------
+	ELSE IF EXISTS(SELECT * FROM inserted) AND EXISTS(SELECT * FROM deleted) BEGIN
+        IF UPDATE([primary]) BEGIN
+            UPDATE a
+            SET [primary] = 0
+            FROM address a
+            JOIN inserted i ON a.client_data_id = i.client_data_id
+            JOIN deleted d ON i.id = d.id
+            WHERE 
+                i.[primary] = 1 AND ISNULL(d.[primary], 0) <> 1 -- zmiana na 1
+                AND a.id <> i.id AND a.[primary] = 1;
+        END
+    END
+
+    --------------------------------------------------------------------------------
+    -- DELETE: jeśli usunięto primary = 1 → wyznacz nowy primary spośród pozostałych
+    --------------------------------------------------------------------------------
+    IF EXISTS(SELECT * FROM deleted) AND NOT EXISTS(SELECT * FROM inserted)
+    BEGIN
+        -- znajdź klientów, którym usunięto główny adres
+        DECLARE @client_data_id INT;
+
+        DECLARE deleted_primary_cursor CURSOR FOR
+        SELECT DISTINCT d.client_data_id
+        FROM deleted d
+        WHERE d.[primary] = 1;
+
+        OPEN deleted_primary_cursor;
+        FETCH NEXT FROM deleted_primary_cursor INTO @client_data_id;
+
+        WHILE @@FETCH_STATUS = 0
+        BEGIN
+            -- wybierz najstarszy pozostały adres i oznacz go jako primary = 1
+            UPDATE address
+            SET [primary] = 1
+            WHERE id = (
+                SELECT TOP 1 id
+                FROM address
+                WHERE client_data_id = @client_data_id
+                ORDER BY id
+            );
+
+            FETCH NEXT FROM deleted_primary_cursor INTO @client_data_id;
+        END
+
+        CLOSE deleted_primary_cursor;
+        DEALLOCATE deleted_primary_cursor;
+    END
+END;
